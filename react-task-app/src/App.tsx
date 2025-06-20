5740f4727318b245b9a57a33ad6dbf024e83542e
sunfivemin <154585975+sunfivemin@users.noreply.github.com>
2025-06-20 16:09:41 +0900

    🎨 LoggerModal 추가

--- react-task-app/src/App.tsx
+++ react-task-app/src/App.tsx
@@ -1,13 +1,21 @@
 import { useState } from 'react';
 import { useTypedSelector } from './hooks/redux.ts';
 import './App.css.ts';
-import { appContainer, board, buttons } from './App.css.ts';
+import {
+  appContainer,
+  board,
+  buttons,
+  deleteBoardButton,
+  loggerButton,
+} from './App.css.ts';
 import BoardList from './components/BoardList/BoardList.tsx';
 import ListContainer from './components/ListsContainer/ListsContainer.tsx';
 import EditModal from './components/EditModal/EditModal.tsx';
+import LoggerModal from './components/LoggerModal/LoggerModal.tsx';
 
 function App() {
   const [activeBoardId, setActiveBoardId] = useState('board-0');
+  const [isLoggerOpen, setLoggerOpen] = useState(false);
   const modalActive = useTypedSelector(state => state.boards.modalActive);
   const boards = useTypedSelector(state => state.boards.boardArray);
 
@@ -20,7 +28,7 @@ function App() {
   return (
     <div className={appContainer}>
       {modalActive ? <EditModal /> : null}
-
+      {isLoggerOpen ? <LoggerModal setLoggerOpen={setLoggerOpen} /> : null}
       <BoardList
         activeBoardId={activeBoardId}
         setActiveBoardId={setActiveBoardId}
@@ -31,8 +39,13 @@ function App() {
       <div className={board}></div>
 
       <div className={buttons}>
-        <button>이 게시판 삭제하기</button>
-        <button>활동 목록 보기</button>
+        <button className={deleteBoardButton}>이 게시판 삭제하기</button>
+        <button
+          className={loggerButton}
+          onClick={() => setLoggerOpen(!isLoggerOpen)}
+        >
+          {isLoggerOpen ? '활동 목록 숨기기' : '활동 목록 보이기'}
+        </button>
       </div>
     </div>
   );
--- react-task-app/src/components/LoggerModal/LoggerModal.tsx
+++ react-task-app/src/components/LoggerModal/LoggerModal.tsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { type FC } from 'react';
+import { useTypedSelector } from '../../hooks/redux';
+import { AiOutlineClose } from 'react-icons/ai';
+import LogItem from './LogItem/LogItem';
+import {
+  body,
+  closeButton,
+  header,
+  modalWindow,
+  title,
+  wrapper,
+} from './LoggerModal.css';
 
-const LoggerModal = () => {
-  return <div>LoggerModal</div>;
+type TLoggerModalProps = {
+  setLoggerOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const LoggerModal: FC<TLoggerModalProps> = ({ setLoggerOpen }) => {
+  const logs = useTypedSelector(state => state.logger.logArray);
+
+  return (
+    <div className={wrapper}>
+      <div className={modalWindow}>
+        <div className={header}>
+          <div className={title}>활동 기록</div>
+          <AiOutlineClose
+            className={closeButton}
+            onClick={() => setLoggerOpen(false)}
+          />
+        </div>
+        <div className={body}>
+          {logs.length === 0
+            ? '활동기록이 없습니다.'
+            : logs.map(log => <LogItem key={log.logId} />)}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoggerModal;
